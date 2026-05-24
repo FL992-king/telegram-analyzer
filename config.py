10b5bbbb6399979a301be9792330bf3d74882444
FL992-king <fabianolorusso.92@gmail.com>
2026-05-24 19:09:57 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -1,15 +1,9 @@
 import os
 
-def get_env(name):
-    value = os.getenv(name)
-    if value is None:
-        raise ValueError(f"Variabile {name} non trovata nei secrets GitHub")
-    return value
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
 
-BOT_TOKEN = get_env("BOT_TOKEN")
-CHAT_ID = get_env("CHAT_ID")
-
-API_ID = int(get_env("API_ID"))
-API_HASH = get_env("API_HASH")
+API_ID = int(os.environ["API_ID"])
+API_HASH = os.environ["API_HASH"]
 
 CHANNEL = "ApplicazioniCR"
